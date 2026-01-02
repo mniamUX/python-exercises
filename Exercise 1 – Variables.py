87e899af9f690dc394c5e52e6749e5d8dd7f935f
LeKKsa <135523692+mniamUX@users.noreply.github.com>
2026-01-02 17:50:12 +0100

    Create Exercise 1 - Variables.py
    This script creates and prints variables related to a browser test.

---
+++ Exercise 1 – Variables.py
@@ -0,0 +1,11 @@
+# create variables ("boxes" with "labels" - it is used to store data)
+browser_name = "Chrome"   # string (text)
+test_version = 1.0        # number (decimal, float)
+is_test_passed = True     # Boolean (True/False - yes/no type of answer)
+# print variables
+print("Browser Name:", browser_name) 
+print("Test Version:", test_version) 
+print("Test Passed:", is_test_passed)
+# Comments explain the code:
+# "Browser Name:" → just some text to explain what we are printing (shown on the screen)
+# browser_name → the actual value stored in the variable
